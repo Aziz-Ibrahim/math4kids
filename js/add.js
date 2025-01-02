723566163e7f41a4168477a11dd9393ef53de25e
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-01-02 04:23:04 +0000

    Update javascript code to run more efficiently

--- js/add.js
+++ js/add.js
@@ -1,19 +1,18 @@
-const option1 = document.getElementById("option1");
-const option2 = document.getElementById("option2");
-const option3 = document.getElementById("option3");
-const audio = document.getElementById("wrong");
+const option1 = document.getElementById("option1"),
+    option2 = document.getElementById("option2"),
+    option3 = document.getElementById("option3"),
+    audio = document.getElementById("wrong");
 let answer = 0;
 
-
 function generateEquation() {
-    let num1 = Math.floor(Math.random() * 13);
-    let num2 = Math.floor(Math.random() * 13);
-    let dummyAnswer1 = Math.floor(Math.random() * 13);
-    let dummyAnswer2 = Math.floor(Math.random() * 13);
-    let allAnswers = [];
-    let switchAnswers = [];
+    let num1 = Math.floor(Math.random() * 13),
+        num2 = Math.floor(Math.random() * 13),
+        dummyAnswer1 = Math.floor(Math.random() * 10),
+        dummyAnswer2 = Math.floor(Math.random() * 10),
+        allAnswers = [],
+        switchAnswers = [];
 
-    answer = num1 + num2;
+    answer = eval(num1 + num2);
 
     document.getElementById("num1").innerHTML = num1;
     document.getElementById("num2").innerHTML = num2;
@@ -22,15 +21,12 @@ function generateEquation() {
 
     for (i = allAnswers.length; i--;) {
         switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
-    }
-
+    };
 
     option1.innerHTML = switchAnswers[0];
     option2.innerHTML = switchAnswers[1];
     option3.innerHTML = switchAnswers[2];
-
-
-}
+};
 
 option1.addEventListener("click", function () {
     if (option1.innerHTML == answer) {
